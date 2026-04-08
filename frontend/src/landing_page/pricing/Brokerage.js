9647a5822d07b3d9cfb3ef94b12513a3b023f2b6
shivratnadhoble <shivratnadhoble9@gmail.com>
2026-04-08 21:46:02 +0530

    Accessibility: Final sweep and fix of all remaining alt props and empty hrefs

--- frontend/src/landing_page/pricing/Brokerage.js
+++ frontend/src/landing_page/pricing/Brokerage.js
@@ -6,7 +6,7 @@ function Brokerage() {
 
             <div className="row p-5 mt-5 text-center border-top">
                 <div className="col-8 p-4">
-                    <a href="" style={{ textDecoration: "none" }}>
+                    <a href="/" style={{ textDecoration: "none" }}>
                         <h3 className="fs-5">
                             Brokerage calculator</h3>
                     </a>
@@ -25,7 +25,7 @@ function Brokerage() {
                     </ul>
                 </div>
                 <div className="col-4 p-4">
-                    <a href="" style={{ textDecoration: "none" }}>
+                    <a href="/" style={{ textDecoration: "none" }}>
                         <h3 className="fs-5">List of charges</h3>
                     </a>
                 </div>
