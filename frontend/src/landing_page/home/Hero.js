296449579660a0bfceaec2f8b035df994a127807
shivratnadhoble <shivratnadhoble9@gmail.com>
2026-04-10 13:14:44 +0530

    Fix ESLint img-redundant-alt error in Hero.js for CI build

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
     return (
         <div className='container p-5 mb-5'>
             <div className='row text-center'>
-                <img src='media/images/homeHero.png' alt='Hero Image' className='mb-5' />
+                <img src='media/images/homeHero.png' alt='Zerodha hero banner' className='mb-5' />
                 <h1 className='mt-5 display-4'>Invest in Everything</h1>
                 <p className='fs-5 text-muted mb-4'>Online platform to invest in stocks, derivatives, mutual funds, and more </p>
                 <Link 
